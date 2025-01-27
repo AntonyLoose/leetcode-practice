2f5bafe1995f5530f83f2c97689385d26a8f57c4
antonyloose <antony35007@gmail.com>
2025-01-28 00:44:34 +1100

    feat: optimal merge sorted

--- merge-sorted.c
+++ merge-sorted.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 void merge_time_efficient(int *nums1, int m, int *nums2, int n) {
   int nums1_pointer = 0;
   int nums2_pointer = 0;
@@ -23,33 +22,45 @@ void merge_time_efficient(int *nums1, int m, int *nums2, int n) {
       }
     }
   }
+}
 
-  for (int i = 0; i < m + n; i++) {
-    nums1[i] = new_arr[i];
+void merge_optimal(int *nums1, int m, int *nums2, int n) {
+  int i = m - 1;
+  int j = n - 1;
+  int k = m + n - 1;
+
+  // Invariants:
+  // k+1 -> length is sorted
+  // 1 -> i is sorted
+  // arr[k+1] >= arr[i]
+  // therefore, if k = i the array is sorted
+  while (i < k) {
+    if (i >= 0 && nums1[i] > nums2[j]) {
+      nums1[k] = nums1[i];
+      i--;
+    } else {
+      nums1[k] = nums2[j];
+      j--;
+    }
+    k--;
   }
 }
 
-/*
- * A space efficient implementation would sort nums1 in place by shifting the
- * remaining nums1 values to the right one and then inserting the next lowest
- * number where the gap was left. Note that the shifting makes the time
- * complexity O(n^2).
- *
- * example:
- * nums1 = [1, 4, 6, 0, 0, 0];
- * nums2 = [2, 3, 7];
- *
- * [1, 4, 6, 0, 0, 0];
- * [1, _, 4, 6, 0, 0] -> [1, 2, 4, 6, 0, 0];
- * [1, 2, _, 4, 6, 0] -> [1, 2, 3, 4, 6, 0];
- * [1, 2, 3, 4, 6, 7];
- */
-
 int main() {
-  int arr1[6] = {1, 2, 3, 0, 0, 0};
+  int arr1[6] = {4, 5, 6, 0, 0, 0};
   int *ptr1 = arr1;
-  int arr2[3] = {2, 5, 6};
+  int arr2[3] = {1, 2, 3};
   int *ptr2 = arr2;
-  merge_time_efficient(ptr1, 3, ptr2, 3);
+  merge_optimal(ptr1, 3, ptr2, 3);
+
+  for (int i = 0; i < 6; i++) {
+    printf("%d", arr1[i]);
+    if (i != 5) {
+      printf(", ");
+    } else {
+      printf("\n");
+    }
+  }
+
   return 1;
 }
