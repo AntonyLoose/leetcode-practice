eb0a1d22600bbac3c5d9af326fa0dfdb76f319dd
Antony <86053464+AntonyLoose@users.noreply.github.com>
2025-04-10 19:00:46 +1000

    feat: two sum II

--- two-pointers/m-two-sum-II.js
+++ two-pointers/m-two-sum-II.js
@@ -1,9 +1,23 @@
 const assert = require('assert');
 
 function twoSum(numbers, target) {
+    let p1 = 0;
+    let p2 = numbers.length - 1;
+    let sum;
 
+    while (sum !== target && p1 < p2){
+        sum = numbers[p1] + numbers[p2];
+        if (sum < target) p1++;
+        if (sum > target) p2--;
+    }
+
+    // output expected in 1-indexed form
+    return [p1+1, p2+1];
 }
 
 // --- Testing --- //
+const numbers = [2, 7, 11, 15];
+const target = 9;
+const expected = [1, 2];
 
-
+assert.deepStrictEqual(expected, twoSum(numbers, target));
