903ab8a77ac21b67b69dfd12a8e05735552fd165
Antony <86053464+AntonyLoose@users.noreply.github.com>
2025-05-12 12:56:47 +1000

    feat: max depth binary tree

---
+++ binary-tree/e-max-depth.ts
@@ -0,0 +1,19 @@
+class TreeNode {
+    val: number
+    left: TreeNode | null
+    right: TreeNode | null
+    constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+        this.val = (val === undefined ? 0 : val)
+        this.left = (left === undefined ? null : left)
+        this.right = (right === undefined ? null : right)
+    }
+}
+
+function maxDepth(root: TreeNode | null, depth = 0): number {
+    if (!root) {
+        return depth;
+    }
+
+    depth++
+    return Math.max(maxDepth(root.left, depth), maxDepth(root.right, depth));
+};
